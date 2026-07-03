9d477fafed08f1354495dd63cb418a39df2f746e
camilaMoralesA <camila.morales03@alumnos.ucn.cl>
2026-07-03 00:46:20 -0400

    creación de la clase trie

---
+++ data_structures/Trie.cpp
@@ -0,0 +1,21 @@
+//
+// Created by camil on 7/3/2026.
+//
+
+#include "Trie.hpp"
+using namespace std;
+Trie::Trie() {
+    root = new NodeTrie();
+}
+
+int Trie::tenerI(char letra) {
+
+}
+
+void Trie::agregar() {
+
+}
+
+Cancion *Trie::buscar() {
+
+}
